d7d50e8219504329bded4ea62dbc8e11c20504d0
jagdishwaghmode <67308544+jagdishwaghmode@users.noreply.github.com>
2026-06-23 21:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (13.67%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -10,38 +10,14 @@
  * };
  */
 class Solution {
-private:
-int levelOrderTraversal(TreeNode* root){
-    int cnt = 0;
-    queue<TreeNode*> q;
-    q.push(root);
-    q.push(NULL);
-    while(!q.empty()){
-        TreeNode* temp = q.front();
-        q.pop();
-        if(temp == NULL){
-            cnt++;
-            if(!q.empty()){
-                q.push(NULL);
-            }
-        }
-        else{
-            if(temp->left){
-                q.push(temp->left);
-            }
-            if(temp->right){
-                q.push(temp->right);
-            }
-        }
-    }
-    return cnt;
-}
 public:
     int maxDepth(TreeNode* root) {
         if(root==NULL){
             return 0;
         }
-        int ans = levelOrderTraversal(root);
-        return ans;
+        int leftcnt = maxDepth(root->left);
+        int rightcnt = maxDepth(root->right);
+
+        return max(leftcnt,rightcnt)+1;
     }
 };
