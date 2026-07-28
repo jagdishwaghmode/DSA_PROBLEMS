5eb3284b018af215cb558cbabf2229256f7451c7
jagdishwaghmode <67308544+jagdishwaghmode@users.noreply.github.com>
2026-07-28 13:59:04 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (40.39%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        mpp = {}
+        
+        for i in range(n):
+            rem = target - nums[i]
+            if rem in mpp:
+                return [mpp[rem], i]
+
+            mpp[nums[i]] = i
+
+        return []
