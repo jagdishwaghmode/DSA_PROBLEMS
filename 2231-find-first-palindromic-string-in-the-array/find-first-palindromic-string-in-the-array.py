778994934ee5e796734fc6902a45ae743bfa6c95
jagdishwaghmode <67308544+jagdishwaghmode@users.noreply.github.com>
2026-07-29 16:09:19 +0530

    Time: 5 ms (38.97%) | Memory: 19.5 MB (6.34%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for i in words:
+            s = i[::-1]
+            if s==i:
+                return i
+
+        return ""
