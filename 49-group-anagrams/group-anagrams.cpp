9c4379bffede1f4bbec524a1f30c5a1e7ce4ff77
jagdishwaghmode <67308544+jagdishwaghmode@users.noreply.github.com>
2026-09-01 23:42:30 +0530

    Time: 24 ms (25.58%) | Memory: 28.1 MB (20.42%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<string> str;
         int n = strs.size();
+        vector<string> s;
         for(int i = 0; i<n; i++){
-            string s = strs[i];
-            sort(s.begin(),s.end());
-            str.push_back(s);
+            string temp = strs[i];
+            sort(temp.begin(),temp.end());
+            s.push_back(temp);
         }
         unordered_map<string,vector<int>> mpp;
         for(int i = 0; i<n; i++){
-            mpp[str[i]].push_back(i);
+            mpp[s[i]].push_back(i);
         }
         vector<vector<string>> ans;
-        
-        for(auto it:mpp){
-            vector<string> res;
-            int n = it.second.size();
-            for(int i = 0; i<n; i++){
-                res.push_back(strs[it.second[i]]);
+        for(auto it: mpp){
+            int x = it.second.size();
+            vector<string> temp2;
+            for(int j = 0; j<x; j++){
+                temp2.push_back(strs[it.second[j]]);
             }
-            ans.push_back(res);
+            ans.push_back(temp2);
+            temp2.clear();
         }
         return ans;
     }
 };
+
+//aet = 0,1,3
